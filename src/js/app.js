7fdd56d56943fefcee34e3e25abd0ad10fbd434e
Sunatullo Gafurov <me@gmail.com>
2020-01-26 12:36:42 +0500

    Initial Version

---
+++ src/js/app.js
@@ -0,0 +1,57 @@
+const rootEl = document.querySelector('#root').innerHTML = `
+    <form>
+        <label><h3>Total</h3></label>
+        <input data-input="input-total" class="form-control" type="number" min="0" value="">
+        <label><h3>Category</h3></label>
+        <input data-input="input-category"class="form-control" type="text" value="">
+        <button data-action="add-button"type="submit" style="margin-top: 10px;" class="btn btn-success btn-block">Add</button>
+    <form>
+    <ul data-ul="lists" style="margin-top: 50px;" class="list-group"></ul>
+    <div style="margin-top: 50px;"><h5 data-h5="total">TOTAL: 0</h5></div>
+`;
+const inputTotal = document.querySelector('[data-input="input-total"]');
+const inputCategory = document.querySelector('[data-input="input-category"]');
+const addForm = document.querySelector('[data-action="add-button"]');
+const lists = document.querySelector('[data-ul="lists"]');
+const total = document.querySelector('[data-h5="total"]');
+let sumOfPurchases = 0;
+
+addForm.addEventListener('click', (e) => {
+    e.preventDefault();
+    const totalValue = inputTotal.value;
+    const categoryValue = inputCategory.value;
+    sumOfPurchases += parseInt(totalValue);
+    const li = document.createElement('li');
+    li.className = 'list-group-item';
+    li.innerHTML = `Purchase for $${totalValue} in ${categoryValue} category
+    <div style="text-align: right;"> 
+        <button class="btn btn-danger remove">x</button>
+        <button class="btn btn-primary move-up">↑</button>
+        <button class="btn btn-primary move-down">↓</button>
+    </div>
+    `;
+    lists.appendChild(li);
+    total.textContent = `TOTAL: $${sumOfPurchases}`;
+    const buttonRemove = li.querySelector('.remove');
+    buttonRemove.addEventListener('click', () => {
+        li.remove();
+        sumOfPurchases -= parseInt(totalValue);
+        total.textContent = `TOTAL: $${sumOfPurchases}`
+    });
+    const buttonUp = li.querySelector('.move-up');
+    buttonUp.addEventListener('click', (e) => {
+        e.preventDefault();
+        lists.insertBefore(li, li.previousSibling);
+    });
+    const buttonDown = li.querySelector('.move-down');
+    buttonDown.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (li == lists.lastChild) {
+            lists.insertBefore(li, lists.firstChild);
+        } else {
+        lists.insertBefore(li.nextSibling, li);
+        }
+    });
+    inputTotal.value = '';
+    inputCategory.value = '';
+});
